7a1284cf0d6c3e5a47945bf84f3dca61f5760658
arsalon <arsalonamini2020@gmail.com>
2021-08-23 12:10:52 +0800

    python variables including naming convention

---
+++ variables.py
@@ -0,0 +1,14 @@
+#naming convention is all lowercase with an underscore
+students_count = 1000
+rating = 4.99
+is_published = False
+course_name = "python"
+
+
+# can initialize multiple variables in the same line
+x = 1
+y = 2
+x, y = 1, 2
+
+#can set a multiple variables to the same value
+x = y = 1
