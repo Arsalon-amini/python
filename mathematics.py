ddc93b0f6bd1c2e9ca0a4e489efe408d442f956a
arsalon <arsalonamini2020@gmail.com>
2021-08-23 23:09:55 +0800

    Mathematics in python - using the math module (object with math methods)

---
+++ mathematics.py
@@ -0,0 +1,8 @@
+import math #an object with a bunch of methods for 
+
+PI = 3.14
+NEGATIVE_PI = -3.14
+print(round(PI))
+print(abs(NEGATIVE_PI))
+z = math.floor(PI)
+print(PI, NEGATIVE_PI, z)
--- numbers.py
+++ numbers.py
@@ -13,8 +13,16 @@ b = hex(55)  #built in fn -> returns hexidecimal representation
 #complex numbers
 c = 1 + 2j #in python 'j' reprseents imaginary number i in mathematics 
 
-
-
 print(x, y, z, a, b, c)
 
+#arithmetic operations
+d = 10 + 3
+e = 10 - 2
+f = 10 * 3
+g = 10 / 2.5    #returns a floating point number
+h = 10 // 2.5   #returns an integer
+i = 10 % 3      #modulous = remaineder or a division 
+j = 10 ** 3  #exponent (left to power right)
 
+k = 1
+k += 1 #don't have x++ or x-- (increment / decrement operators)
