9b78c6b77fcadcc3fbf5bcd93eb48a8b13c3d2d5
arsalon <arsalonamini2020@gmail.com>
2021-08-23 23:19:51 +0800

    conditional statements - if else - using indendations vs. curly braces

---
+++ conditionals.py
@@ -0,0 +1,12 @@
+age = 22
+
+# no curly brances, an indentation signifies a code block
+#pep 8 specifies four spaces for indentation (cannot mix tab and four spaces)
+
+if age >= 18:
+    print("This is an adult")
+    print("this is an adult")
+elif age >= 13:
+    print("this is a teenager")
+else:
+    print("done")
