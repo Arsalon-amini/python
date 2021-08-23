29a5feb9e3221e59f6188b260e5ef37fe432dd0e
arsalon <arsalonamini2020@gmail.com>
2021-08-23 22:42:20 +0800

    numbers in python (binary, hexidecimals, complex numbers)

---
+++ numbers.py
@@ -0,0 +1,20 @@
+#representing numbers (dec, binary, hexidecimal, complex nums)
+
+#binary
+x = 10
+y = 0b10  #number 2 in binary
+z = bin(256)  #takes a number, returns binary representation
+
+#hexidecimal
+a = 0x12c #number 300 in hexidecmial system
+b = hex(55)  #built in fn -> returns hexidecimal representation 
+
+
+#complex numbers
+c = 1 + 2j #in python 'j' reprseents imaginary number i in mathematics 
+
+
+
+print(x, y, z, a, b, c)
+
+
