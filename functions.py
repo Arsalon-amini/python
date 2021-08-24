213ff900e2945384dfa1cc1a24b25377654fe278
arsalon <arsalonamini2020@gmail.com>
2021-08-24 12:43:03 +0800

    functions in python

---
+++ functions.py
@@ -0,0 +1,38 @@
+#functions in python
+
+def my_function(number, another_number):
+    return number + another_number
+
+
+output = my_function(2, 3)
+print(output)
+
+#return multiple things from a function
+def my_new_function(number, another_number):
+    return (number, number + another_number)
+
+output_two = my_new_function(5, 3)
+print(output_two)
+
+#keyword args 
+def my_alt_function(number, another_number):
+    return number + another_number
+
+#makes code more readable
+output_three = my_alt_function(3, another_number=3)
+print(output_three)
+
+
+#assign default values 
+def function (number, another_number = 5):
+    return number + another_number
+
+#don't have to pass a second argument 
+output_four = function(3)
+print(output_four)
+
+#type hinting showing the types for args and type for return values (after the -> )
+def function_hints(number: int, another_number: int = 3) -> int:
+    return number + another_number
+
+print(function_hints(3))
