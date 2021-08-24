3e6c5a9fb9a1302675bb811a557bccb3d96bee8a
arsalon <arsalonamini2020@gmail.com>
2021-08-24 11:00:08 +0800

    Falsy values and explicit type conversion in python

---
+++ falsy.py
@@ -0,0 +1,9 @@
+#falsy values
+
+y = []
+z = ''
+x = 0
+y = None
+
+
+print(bool(y), bool(z), bool(x), bool(y))
---
+++ type_conversion.py
@@ -0,0 +1,9 @@
+x = input("x: ")
+
+# have to explicitly tell python what you want your objects to be converted into 
+print(int(x))
+print(float(x))
+print(bool(x))
+
+
+
