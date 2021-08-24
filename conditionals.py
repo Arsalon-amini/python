28b2c374a5ef2700d5dcd3082dfebde09b01f0c6
arsalon <arsalonamini2020@gmail.com>
2021-08-24 11:13:34 +0800

    ternary operatory in python

--- conditionals.py
+++ conditionals.py
@@ -1,7 +1,7 @@
 age = 22
 
 # no curly brances, an indentation signifies a code block
-#pep 8 specifies four spaces for indentation (cannot mix tab and four spaces)
+# pep 8 specifies four spaces for indentation (cannot mix tab and four spaces)
 
 if age >= 18:
     print("This is an adult")
@@ -10,3 +10,9 @@ elif age >= 13:
     print("this is a teenager")
 else:
     print("done")
+
+# ternary operatory in Java
+# message= age >= 18 ? "Eligable": "Not Eligable")
+
+# ternary operator in python
+message = "Teenager" if age >= 18 else "Adult"
